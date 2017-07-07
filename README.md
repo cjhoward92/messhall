60e931d51beb130c1a17c746c4143caa7475c26d
Carson Howard <carsonh@axosoft.com>
2017-07-06 22:25:23 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+[link1](http://www.linuxhowtos.org/C_C++/socket.)
+
+[code](http://www.linuxhowtos.org/data/6/server.c)
--- queue.c
+++ queue.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include "utils.h"
 
 int main() {
